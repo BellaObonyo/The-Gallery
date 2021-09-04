c397930716b9588d90845a89fb20173762b32e4e
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-04 21:53:26 +0300

    added another model

--- gallery/photos/admin.py
+++ gallery/photos/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Category, Editor,pub_date, Location, name_image
+from .models import Category, Editor, Location,Image
 
 admin.site.register(Editor)
 admin.site.register(Location)
 admin.site.register(Category)
-admin.site.register(pub_date)
-admin.site.register(name_image)
+admin.site.register(Image)
+
