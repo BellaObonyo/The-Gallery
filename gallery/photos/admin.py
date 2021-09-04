b495c26985ee55d17669d00763cf1a4ba3eaf3ac
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-04 21:44:37 +0300

    added models to my admin.py file

--- gallery/photos/admin.py
+++ gallery/photos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Category, Editor,pub_date, Location, name_image
+
+admin.site.register(Editor)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(pub_date)
+admin.site.register(name_image)
--- gallery/photos/models.py
+++ gallery/photos/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+class Image(models.Model):
+    description= models.CharField(max_length =60)
+    editor = models.ForeignKey('Editor', on_delete=models.CASCADE)
+    location = models.ManyToManyField('Location')
+    category = models.ManyToManyField('Category')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    name_image = models.ImageField(upload_to = 'articles/', default='default.jpg')
+
+    def __str__(self):
+        return self.description
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
+
+    def save_editor(self):
+        self.save()
+
+    def __str__(self):
+        return self.first_name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    class Meta:
+        verbose_name_plural='CATEGORIES'
+
+    def __str__(self):
+        return self.name
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    class Meta:
+        verbose_name_plural='LOCATIONS'
+
+    def __str__(self):
+        return self.name
+
