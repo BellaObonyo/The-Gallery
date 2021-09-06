632fc08f5445af15ed9985b921acbc2b10c70de4
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-06 12:43:34 +0300

    updated html file

--- gallery/photos/views.py
+++ gallery/photos/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import redirect, render
 from photos.models import Image
 # Create your views here.
 def index(request):
-    Jane=Image.get_name()
-    return render(request,'index.html',{"Jane":Jane})
+    images=Image.objects.all()
+    return render(request,'index.html',{"images":images})
