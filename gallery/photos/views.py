5a917f66efd5f0b4b94b69bba3746285edee42b9
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-06 13:05:20 +0300

    updated views.py

--- gallery/photos/views.py
+++ gallery/photos/views.py
@@ -5,3 +5,16 @@ from photos.models import Image
 def index(request):
     images=Image.objects.all()
     return render(request,'index.html',{"images":images})
+
+    
+def search_photos_category(request):
+  if 'photo' in request.GET and request.GET["photo"]:
+    search_term = request.GET.get("photo")
+    searched_photos = Image.search_photos_by_category(search_term)
+    message = f"{search_term}"
+
+    return render(request, 'search.html', {"message":message,"photos":searched_photos})
+
+  else:
+    message = 'You have not searched for any term'
+    return render(request, 'search.html', {"message":message})    
