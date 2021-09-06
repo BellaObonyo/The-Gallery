788ff7ec50394881d774d9a55038346ba9ad096c
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-06 15:31:57 +0300

    updated  navbar file

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -47,7 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photos'
+    'photos',
+    'bootstrap5'
+    
 ]
 
 MIDDLEWARE = [
