4c9e502bad99f74df6534d5c9d4437cfbb0de520
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-06 17:07:01 +0300

    updated settings.py file

--- gallery/photos/models.py
+++ gallery/photos/models.py
@@ -11,6 +11,13 @@ from django.db.models.deletion import CASCADE
 class Category(models.Model):
     name = models.CharField(max_length =30)
 
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+        
     class Meta:
         verbose_name_plural='CATEGORIES'
 
@@ -18,6 +25,12 @@ class Category(models.Model):
         return self.name
 class Location(models.Model):
     name = models.CharField(max_length =30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
 
     class Meta:
         verbose_name_plural='LOCATIONS'
@@ -30,16 +43,30 @@ class Image(models.Model):
     location = models.ForeignKey(Location,on_delete=CASCADE)
     category = models.ForeignKey(Category,on_delete=CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
-    image =models.ImageField(upload-To,default=True)  
+    image = models.ImageField(upload_to='gallery/',default='no photo')  
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
 
     @classmethod
     def get_name(cls):
         Bella=cls.objects.all()
         return Bella
+
     def __str__(self):
         return self.description
+
     @classmethod
     def search_by_category(cls,category):
         images=cls.objects.filter(image_category__name__icontains=category)
         return images
+    
+    @classmethod
+    def get_image_by_id(cls, id):
+        image_id=cls.objects.filter(id=id).all()
+        return image_id
+
 
--- gallery/photos/tests.py
+++ gallery/photos/tests.py
@@ -1,3 +1,70 @@
+from os import name
+from gallery.photos.models import Category, Image, Location
 from django.test import TestCase
 
 # Create your tests here.
+class testImage(TestCase):
+    def setUp(self):
+        self.location=Location(name='Grill')
+        self.location.save_location()
+
+        self.category=Category(name='Jill')
+        self.category.save_category()
+
+        self.image=Image(id=1,description='longblue',location=self.location,category=self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def test_save_image(self):
+        self.image.save_image()
+        saved=Image.objects.all()
+        self.assertTrue(len(saved) > 0)   
+
+    def test_delete_image(self):
+        self.image.delete_image()
+        deleted=Image.objects.all()
+        self.assertTrue(len(deleted) == 0) 
+
+    def test_search_by_category(self):
+        image_category=''
+        result=self.image.search_by_category(image_category)
+        self.assertTrue(len(result) >=0)
+
+class TestCategory(TestCase):
+    def setUp(self):
+        self.image_category=Category(name='violet')
+        self.image_category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_category, Category))
+
+    def test_save_category(self):
+        self.image_category.save_category()
+        category=Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+    def test_delete_category(self):
+        self.image_category.delete_category()
+        category=Category.objects.all()
+        self.assertTrue(len(category) ==0)
+
+class TestLocation(TestCase):
+    def setUp(self):
+        self.image_location=Location(name='nature')
+        self.image_location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_location, Location))
+
+    def test_save_location(self):
+        self.image_location.save_location()
+        location=Location.objects.all()
+        self.assertTrue(len(location) > 0)
+
+    def test_delete_location(self):
+        self.image_location.delete_location()
+        location=Location.objects.all()
+        self.assertTrue(len(location) ==0)
+
+
